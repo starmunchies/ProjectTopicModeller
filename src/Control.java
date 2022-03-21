65f4a7dc479f5031e4544695e8fb9d5f9bbf5dc1
Starmunchies <c20336046@mytudublin.ie>
2022-03-21 12:52:21 +0000

    added file chooser
    created a test file chooser based of java doc tutorials i found and integrated it into main ui

--- src/Control.java
+++ src/Control.java
@@ -5,18 +5,20 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
 
-import javax.swing.*;  
+import javax.swing.*;
 
 public class Control {
-	public static String[] fileaddresses = { "bob.txt", "happy.txt" };
+	//public static String[] fileaddresses = {};
 	public static String noprefix = null;
+	static ArrayList<String> fileaddresses = new ArrayList<String>();
 
 	public static void main(String[] args) {
 
 		getui();
-		
-		
+
 		// in order to get an input array i intend to get the user to either drag and
 		// drop a folder into the window
 		// or open it from a folder which will give me the address location
@@ -27,48 +29,47 @@ public class Control {
 		// not entirely sure how i will do that
 
 		// need a dictionary to get most top common words
-		// need to figure out how to sort a dictionary the stream() function exists think i can use that
-		
-
-		
+		// need to figure out how to sort a dictionary the stream() function exists
+		// think i can use that
 
 	}
+
 	/**
-	 *  this initializes the ui interface such as the buttons the file explorer
-	 *  
+	 * this initializes the ui interface such as the buttons the file explorer
+	 * 
 	 */
-	public static void getui()
-	{
-		JFrame f=new JFrame("Topic Modeller");//creating instance of JFrame  
-		
-		JButton b=new JButton("Add File");//creating instance of JButton  
-		b.setBounds(70,400,100, 40);//x axis, y axis, width, height 
-		
-		JButton a=new JButton("Examine");//creating instance of JButton  
-		a.setBounds(230,400,100, 40);//x axis, y axis, width, height  
-		
-		JButton c=new JButton("Save Results");//creating instance of JButton  
-		c.setBounds(230,20,140, 40);//x axis, y axis, width, height 
-		
+	public static void getui() {
+		JFrame f = new JFrame("Topic Modeller");// creating instance of JFrame
+
+		JButton b = new JButton("Add File");// creating instance of JButton
+		b.setBounds(70, 400, 100, 40);// x axis, y axis, width, height
+
+		JButton a = new JButton("Examine");// creating instance of JButton
+		a.setBounds(230, 400, 100, 40);// x axis, y axis, width, height
+
+		JButton c = new JButton("Save Results");// creating instance of JButton
+		c.setBounds(230, 20, 140, 40);// x axis, y axis, width, height
+
 		JLabel l = new JLabel("<html>Hello testing testing <br>1 2 3 seeing how long before</html? ");
 		l.setBounds(0, -100, 400, 350);
-	
-		f.add(l);//adding button in JFrame        
-		f.add(c);//adding button in JFrame          
-		f.add(b);//adding button in JFrame  
-		f.add(a);//adding button in JFrame  
-		f.setSize(400,500);//400 width and 500 height  
-		f.setLayout(null);//using no layout managers  
-		f.setVisible(true);//making the frame visible 
-		
+
+		f.add(l);// adding button in JFrame
+		f.add(c);// adding button in JFrame
+		f.add(b);// adding button in JFrame
+		f.add(a);// adding button in JFrame
+		f.setSize(400, 500);// 400 width and 500 height
+		f.setLayout(null);// using no layout managers
+		f.setVisible(true);// making the frame visible
+
 		a.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				ConvertArrays convertedarrays = new ConvertArrays(fileaddresses);
+				String newfileaddresses[] = fileaddresses.toArray(new String[fileaddresses.size()]);
+				ConvertArrays convertedarrays = new ConvertArrays(newfileaddresses);
 				String[][] converted = convertedarrays.toconvert();
-				
+
 				// System.out.println(converted[1][2]);
 
 				// this creates an instance of the intersection class
@@ -82,29 +83,43 @@ public class Control {
 				// gonna have to construct a 2d arrays of the array created
 				// pass it through and reconstruct it on the other side
 
-				int filecount = fileaddresses.length;
+				int filecount = newfileaddresses.length;
 				intersect createintersection = new intersect(converted, filecount);
 
 				noprefix = createintersection.getIntersection();
 				int count = createintersection.getCount();
 				System.out.println(count);
-				
 
 				if (count == 0) {
-
+					l.setText("There are no common topics between these files");
 					System.out.println("There are no common topics between these files");
 				} else {
+					l.setText("Your files have something in common!!");
 					System.out.println("Your files have something in common!!");
 				}
-				
+
 			}
 		});
-		
+
 		b.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
+				
+
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+				int result = fileChooser.showOpenDialog(f);
+				
+				if (result == JFileChooser.APPROVE_OPTION) 
+				{
+					File selectedFile = fileChooser.getSelectedFile();
+					fileaddresses.add(selectedFile.getAbsolutePath());  
+					
+					System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+					
+				}
 			}
 		});
 	}
